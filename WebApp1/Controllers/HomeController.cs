cd8a4624344ccbdf109b50af48ec9161a8f479f8
Beyler55 <baylarss@code.edu.az>
2022-10-04 14:07:17 +0400

    intro

---
+++ WebApp1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Baylar";
+            ViewBag.Surname = "Sadygov";
+            ViewBag.Age = 20;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "+"+994551234567;
+            return View();
+        }
+    }
+}
